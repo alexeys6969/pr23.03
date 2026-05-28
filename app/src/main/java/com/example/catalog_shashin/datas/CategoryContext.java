1ea7adbdacdb5988c4d10d4a9201efc9cd2c242d
gschhh <alexshashin.2023@gmail.com>
2026-05-28 12:24:27 +0500

    Разработанные классы для категорий.Шашин

---
+++ app/src/main/java/com/example/catalog_shashin/datas/CategoryContext.java
@@ -0,0 +1,19 @@
+package com.example.catalog_shashin.datas;
+
+import com.example.catalog_shashin.domains.models.Category;
+import com.example.catalog_shashin.domains.models.Сategory;
+
+import java.util.ArrayList;
+
+public class CategoryContext {
+    public static ArrayList<Category> allCategory() {
+        ArrayList<Category> categories = new ArrayList<>();
+
+        categories.add(new Category(-1, "Все"));
+        categories.add(new Category(0, "Мужское"));
+        categories.add(new Category(1, "Женское"));
+        categories.add(new Category(2, "Unisex"));
+
+        return categories;
+    }
+}
---
+++ app/src/main/java/com/example/catalog_shashin/domains/models/Category.java
@@ -0,0 +1,12 @@
+package com.example.catalog_shashin.domains.models;
+
+import java.util.Locale;
+
+public class Category {
+    public Integer id;
+    public String name;
+    public Category(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
